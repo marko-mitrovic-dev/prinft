f49bef0edc0ed628ab3b9b92daeae1a39609dcef
Mitre341 <emarkomitrovic@gmail.com>
2026-08-12 14:16:09 +0200

    ft_PRINTF.C  second try

--- ft_printf.c
+++ ft_printf.c
@@ -1,34 +1,47 @@
 #include "ft_printf.h"
 #include <stdarg.h>
 
-int ft_printf(const char *, ...)
+int ft_printf(const char *str, ...)
 {
     int i;
     int letter_counter;
+    va_list args;
+
+    va_start(args, str);
 
     i = 0;
     letter_counter = 0; 
 
-    if (char * == NULL)
-        return(NULL); // ili treba 0 jer je int? 
+    if (str == NULL)
+        return(-1); // ili treba 0 jer je int? 
 
-    while ( char *[i] != '\0')
+    while ( str[i] != '\0')
     {
-       if (char *[i] != '%')
+       if (str[i] != '%')
        {
-        write(1, '%', 1);
-       }  else if (char *[i] == '%' && char *[i + 1] == 'c')
-       {
-        write(1, va_arg, 1);
+        write(1, &str[i], 1);
         letter_counter++;
-       } else if (char *[i] == '%' && char *[i + 1] == '%')
+       }  else if (str[i] == '%' && str[i + 1] == 'c')
        {
-        write(1, '%', 1);
+        ft_putchar_len(va_arg(args, int));
         letter_counter++;
+        i++;
+       } else if (str[i] == '%' && str[i + 1] == '%')
+       {
+        write(1, &str[i], 1);
+        letter_counter++;
+        i++;
        }
 
-
+       i++;
     }
+    (args);
 
     return(letter_counter);
     }
+
+
+
+    /*
+    standard printf returns -1 if forwarded invalid pointer
+     */
