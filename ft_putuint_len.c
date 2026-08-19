aeb0b21cb7420c21286b9814968d8622b69657a4
Mitre341 <emarkomitrovic@gmail.com>
2026-08-19 20:34:32 +0200

    first try

--- ft_putuint_len.c
+++ ft_putuint_len.c
@@ -1,6 +1,24 @@
 #include "printf.h"
 
-int ft_putuint_len(int n)
+int ft_putuint_len(unsigned int n)
 {
+    unsigned int len;
     
+    len = 0;
+    if  (n > 9)
+    {
+        len += ft_putuint_len(n / 10); 
+		len += ft_putuint_len(n % 10);
+    }
+    else
+        len += ft_putchar_len(n + '0');
+    
+    return(len);
 }
+
+/*
+uint - unsigned int -
+ just positive numbers (0 included) 0 up to 4,294,967,295
+
+
+*/
