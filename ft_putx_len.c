028bf7b80d7a73827bcf4782399845f3091a867f
Mitre341 <emarkomitrovic@gmail.com>
2026-08-20 18:02:16 +0200

    edited

--- ft_putx_len.c
+++ ft_putx_len.c
@@ -1,7 +1,24 @@
 #include "printf.h"
 
-int ft_putx_len.c(int c)
+int ft_putx_len(unsigned int n)
 {
-
+    int len;
+    
+    len = 0;
+    if  (n > 15)
+    {
+        len += ft_putuint_len(n / 16); 
+		len += ft_putuint_len(n % 16);
+    }
+    else if (n <= 9)
+    {
+        len += ft_putchar_len(n + '0');
+    }
+    else
+    {
+        len += ft_putchar_len(n - 10 + 'a');
+    }
+    
+    return(len);
     
 }
