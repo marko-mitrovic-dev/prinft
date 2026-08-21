f7b96a5f7e41813fb12a3c50ef761a7741360916
Mitre341 <emarkomitrovic@gmail.com>
2026-08-21 18:09:16 +0200

    some small changes

--- ft_putp_len.c
+++ ft_putp_len.c
@@ -1,14 +1,13 @@
 #include "ft_printf.h"
 
-
-static int ft_putptr_hex(unsigned long ptr)
+static	int	ft_putptr_hex(unsigned long ptr)
 {
-	int len;
+	int	len;
 
 	len = 0;
-	if  (ptr > 15)
+	if (ptr > 15)
 	{
-		len += ft_putptr_hex(ptr / 16); 
+		len += ft_putptr_hex(ptr / 16);
 		len += ft_putptr_hex(ptr % 16);
 	}
 	else if (ptr <= 9)
@@ -16,33 +15,30 @@ static int ft_putptr_hex(unsigned long ptr)
 		len += ft_putchar_len(ptr + '0');
 	}
 	else
-			len += ft_putchar_len(ptr - 10 + 'a');
-	
-	return(len);
+		len += ft_putchar_len(ptr - 10 + 'a');
+	return (len);
 }
 
-int ft_putp_len(unsigned long ptr)
+int	ft_putp_len(unsigned long ptr)
 {
-	int len;
+	int	len;
 
 	len = 0;
 	if (!ptr)
 	{
 		write(1, "(nil)", 5);
 		return (5);
-
 	}
-	
 	len = ft_putstr_len("0x");
 	len += ft_putptr_hex(ptr);
-	
-	return(len);
+	return (len);
 }
-
 
 /*
 NULL is an adress pointing nowhere has value of 0
-null is just a text which printf prints if I send NULL pointer with %s (specificator) 
-nil is just a text which printf prints, if I send NULL pointer with %p (specificator) 
+null is just a text which printf prints,
+if I send NULL pointer with %s (specificator) 
+nil is just a text which printf prints,
+if I send NULL pointer with %p (specificator) 
 nil is short of latin nihil = nothing
 */
